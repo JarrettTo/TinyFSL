c5edb334d41105f37f7134563b64db10f1ce002c
Justin To <104493864+JarrettTo@users.noreply.github.com>
2024-02-20 23:26:35 +0800

    Update iv4_main.py

--- iv4_main.py
+++ iv4_main.py
@@ -33,10 +33,10 @@ def main():
 
     # Here you would add the rest of your script logic,
     # using args.data and args.size where necessary
-    base_dir = './dataset/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/fullFrame-210x260px/{args.data}/'
+    base_dir = './dataset/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/fullFrame-210x260px/{}/'.format(args.data)
 
     # Load the CSV file and split the data
-    csv_file_path = './dataset/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/PHOENIX-2014-T.{args.data}.corpus.csv'  # Update with the actual path
+    csv_file_path = './dataset/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/annotations/manual/PHOENIX-2014-T.{}.corpus.csv'.format(args.data)  # Update with the actual path
     data = pd.read_csv(csv_file_path, sep='|')  # Use the correct separator
     split_data = data
     data_to_save = []
@@ -46,7 +46,7 @@ def main():
     speakers = split_data['speaker'].tolist()
     sequence_glosses = split_data['orth'].tolist()
     text_translations = split_data['translation'].tolist()
-    prefix = "./dataset/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/fullFrame-210x260px/{args.data}/"
+    prefix = "./dataset/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/features/fullFrame-210x260px/{}/".format(args.data)
     image_sequence_paths = [path.replace('/1', '') for path in image_sequence_paths_temp]
 
     base_model = create_model(weights='imagenet', include_top=False)
